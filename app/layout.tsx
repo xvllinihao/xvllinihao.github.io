f2e32e345efd6ecc19978e7302288aa0e5502c23
xvllinihao <lixu_work@yahoo.com>
2023-10-14 20:41:56 +0200

    add auto zoom in

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
  * @Author: xvllinihao lixu_work@yahoo.com
  * @Date: 2023-10-14 13:01:04
  * @LastEditors: xvllinihao lixu_work@yahoo.com
- * @LastEditTime: 2023-10-14 20:29:54
+ * @LastEditTime: 2023-10-14 20:41:03
  * @FilePath: \xvllinihao.github.io\app\layout.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,16 +14,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // const containerStyles = {
-  //   maxWidth: "600px",
-  //   padding: "30px",
-  //   margin: "0 auto", // Center the container horizontally
-  // };
+  const containerStyles = {
+    maxWidth: "600px",
+    padding: "30px",
+    margin: "0 auto", // Center the container horizontally
+    transform: "scale(1.5)", // Zoom content to 150%
+    transformOrigin: "top", // Set the zoom origin to the top
+  };
 
   return (
     <html lang="en">
       <body className="bg-jin-blue min-h-fit">
-        <div className="bg-jin-white flex-col flex gap-2 mx-auto container">
+        <div className="bg-jin-white flex-col flex gap-2 mx-auto container" style={containerStyles}>
           <MyHeader />
           {children}
         </div>
